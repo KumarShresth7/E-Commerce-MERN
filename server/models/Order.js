75831231bb156b9ccf5d22f3d5e08a215a6f0d1b
Kumar Shresth <kumarshresth2004@gmail.com>
2024-07-07 10:29:18 +0530

    models created

---
+++ server/models/Order.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const OrderSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  orderItems: [
+    {
+      name: { type: String, required: true },
+      qty: { type: Number, required: true },
+      price: { type: Number, required: true },
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true,
+      },
+    },
+  ],
+  totalPrice: { type: Number, required: true },
+  isPaid: { type: Boolean, required: true, default: false },
+});
+
+module.exports = mongoose.model('Order', OrderSchema);
