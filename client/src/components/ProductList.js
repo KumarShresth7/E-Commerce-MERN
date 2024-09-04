ad968fce389e72848503bb2743a841973c0f39cc
Kumar Shresth <kumarshresth2004@gmail.com>
2024-09-04 14:39:12 +0530

    #

--- client/src/components/ProductList.js
+++ client/src/components/ProductList.js
@@ -1,30 +1,33 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import Product from './Product';
-import { baseUrl } from '../baseUrl';
-import './styles/ProductList.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Product from "./Product";
+import { baseUrl } from "../baseUrl";
+import "./styles/ProductList.css";
 
 const ProductList = ({ addToCart }) => {
   const [products, setProducts] = useState([]);
-  const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState("");
 
   useEffect(() => {
-    axios.get(`${baseUrl}/api/products`)
-      .then(response => setProducts(response.data))
-      .catch(error => console.error(error))
+    axios
+      .get(`${baseUrl}/api/products`)
+      .then((response) => setProducts(response.data))
+      .catch((error) => console.error(error));
   }, []);
 
   const handleSearchChange = (event) => {
     setSearchQuery(event.target.value);
   };
 
-  const filteredProducts = products.filter(product =>
+  const filteredProducts = products.filter((product) =>
     product.name.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
   return (
     <div className="product-list-container mt-5">
-      <div className="search-bar mb-4">
+      <div className="search-bar mb-6">
+        <i className="search-icon fas fa-search"></i>{" "}
+        {/* Font Awesome search icon */}
         <input
           type="text"
           className="form-control"
@@ -33,8 +36,9 @@ const ProductList = ({ addToCart }) => {
           onChange={handleSearchChange}
         />
       </div>
+
       <div className="row">
-        {filteredProducts.map(product => (
+        {filteredProducts.map((product) => (
           <div key={product._id} className="col-md-4">
             <Product product={product} addToCart={addToCart} />
           </div>
